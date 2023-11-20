fef8ee2790820ecad639124608733a291c349a1c
JoceChang <jocelyn.h.chang@gmail.com>
2023-11-20 14:47:07 -0500

    Add rotation to camera
    Holding right click on mouse

--- FengShui/Assets/CameraMovement.cs
+++ FengShui/Assets/CameraMovement.cs
@@ -10,6 +10,10 @@ public class CameraScript : MonoBehaviour
         
     }
 
+    public float speed = 1.0f;
+	private float X;
+	private float Y;
+
     // Update is called once per frame
     void Update()
     {
@@ -27,5 +31,12 @@ public class CameraScript : MonoBehaviour
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
         movement.Normalize(); //movement speed will be the same in every direction
         transform.Translate(movement * 5.0f * Time.deltaTime);
+        
+        if(Input.GetMouseButton(0)) {
+			transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
+			X = transform.rotation.eulerAngles.x;
+			Y = transform.rotation.eulerAngles.y;
+			transform.rotation = Quaternion.Euler(X, Y, 0);
+		}
     }
 }
