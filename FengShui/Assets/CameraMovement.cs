b467ab26300c54695a38b7c9897a003f4f7dec15
quinnwall <136604168+quinnwall@users.noreply.github.com>
2023-11-20 15:03:14 -0500

    Updates to Camera Movement
    Udpates the camera movement by changing speeds and a commented out up-down movement using space bar and the left shift key.

--- FengShui/Assets/CameraMovement.cs
+++ FengShui/Assets/CameraMovement.cs
@@ -10,33 +10,40 @@ public class CameraScript : MonoBehaviour
         
     }
 
-    public float speed = 2.0f;
+    public float speed = 6.0f;
 	private float X;
 	private float Y;
 
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.W) == true) {
-            gameObject.transform.position = new Vector3(1.0f, 0, 1.0f);
-        }
-
-        if (Input.GetKeyDown(KeyCode.S) == true) {
-            gameObject.transform.position = new Vector3(-1.0f, 0, -1.0f);
-        }
-        */
+        // WASD movements
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
         movement.Normalize(); //movement speed will be the same in every direction
-        transform.Translate(movement * 5.0f * Time.deltaTime);
+        transform.Translate(movement * 6.0f * Time.deltaTime);
         
+        //camera rotation
+        //rotates the camera based on cursor movement when left clicking
         if(Input.GetMouseButton(0)) {
 			transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
 			X = transform.rotation.eulerAngles.x;
 			Y = transform.rotation.eulerAngles.y;
 			transform.rotation = Quaternion.Euler(X, Y, 0);
 		}
+
+        /* up-down movements
+           space bar is up
+           shift key is down
+        
+        if(Input.GetKey("space")){
+            transform.Translate(new Vector3(0, 12f* Time.deltaTime, 0));
+        }
+        if(Input.GetKey(KeyCode.LeftShift)){
+            transform.Translate(new Vector3(0, -12f* Time.deltaTime, 0));
+        }
+        */
     }
 }
